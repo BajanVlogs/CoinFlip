0489315262f2ca86d449a8336cc55e062f7a9e0a
BajanVlogs <kjolly544@gmail.com>
2024-01-25 16:21:59 -0500

    Update CoinFlipCommand.php

--- src/BajanVlogs/CoinFlipCommand.php
+++ src/BajanVlogs/CoinFlipCommand.php
@@ -13,8 +13,7 @@ class CoinFlipCommand extends VanillaCommand {
         parent::__construct(
             $name,
             "CoinFlip",
-            "/cf <player> <betMoney> | /cf <accept/deny>"
-        );
+            "/cf <player> <betMoney> | /cf <accept/deny>");
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args){
@@ -23,34 +22,19 @@ class CoinFlipCommand extends VanillaCommand {
             return false;
         }
 
-        if(count($args) > 0){
-            $subCommand = strtolower(array_shift($args));
-
-            switch($subCommand){
+        $args[0] = strtolower($args[0]);
+        if(isset($args[0])){
+            switch($args[0]){
                 case "accept":
                 case "a":
-                    // Existing code...
+                    $this->handleAccept($sender);
                     break;
                 case "deny":
                 case "d":
-                    // Existing code...
+                    $this->handleDeny($sender);
                     break;
                 default:
-                    if(count($args) > 0){
-                        $betAmount = abs(array_shift($args));
-                        $targetPlayerName = array_shift($args);
-
-                        $targetPlayer = $sender->getServer()->getPlayer($targetPlayerName);
-
-                        if($targetPlayer === null){
-                            $sender->sendMessage("Player is not online.");
-                            break;
-                        }
-
-                        // Existing code...
-                    } else {
-                        $sender->sendMessage("USAGE: /cf <player> <betMoney> | /cf <accept/deny>");
-                    }
+                    $this->handleCoinFlip($sender, $args);
                     break;
             }
         } else {
@@ -58,5 +42,77 @@ class CoinFlipCommand extends VanillaCommand {
         }
 
         return true;
+    }
+
+    private function handleAccept(Player $sender){
+        if(isset(CoinFlip::$queue[strtolower($sender->getName())])){
+            $this->resolveCoinFlip($sender, true);
+        } else {
+            $sender->sendMessage(TextFormat::RED . "There are no CoinFlip requests enqueued for you right now.");
+        }
+    }
+
+    private function handleDeny(Player $sender){
+        if(isset(CoinFlip::$queue[strtolower($sender->getName())])){
+            $this->resolveCoinFlip($sender, false);
+        } else {
+            $sender->sendMessage(TextFormat::RED . "There are no CoinFlip requests enqueued for you right now.");
+        }
+    }
+
+    private function handleCoinFlip(Player $sender, array $args){
+        if(isset($args[1])){
+            $args[1] = abs($args[1]);
+            $player = $sender->getServer()->getPlayer($args[0]);
+            if($player === null){
+                $sender->sendMessage("Player is not online.");
+                return;
+            }
+
+            if(EconomyAPI::getInstance()->myMoney($player->getName()) < $args[1]){
+                $sender->sendMessage(TextFormat::RED . $player->getName() . " does not have enough money.");
+                return;
+            }
+
+            if(EconomyAPI::getInstance()->myMoney($sender->getName()) < $args[1]){
+                $sender->sendMessage(TextFormat::RED . "Not enough money.");
+                return;
+            }
+
+            CoinFlip::$queue[strtolower($player->getName())] = [strtolower($sender->getName()), $args[1]];
+            CoinFlip::$inp[strtolower($sender->getName())] = true;
+
+            $player->sendMessage(TextFormat::GOLD . $sender->getName() . " requested a $" . $args[1] . " CoinFlip with you.");
+            $player->sendMessage(TextFormat::GOLD . "Type '/cf accept' to accept CoinFlip");
+            $player->sendMessage(TextFormat::GOLD . "Type '/cf deny' to deny CoinFlip");
+            $sender->sendMessage(TextFormat::GREEN . "$" . $args[1] . " CoinFlip Requested!");
+        } else {
+            $sender->sendMessage("USAGE: /cf <player> <betMoney> | /cf <accept/deny>");
+        }
+    }
+
+    private function resolveCoinFlip(Player $sender, bool $accepted){
+        $opponentName = CoinFlip::$queue[strtolower($sender->getName())][0];
+        $betAmount = CoinFlip::$queue[strtolower($sender->getName())][1];
+
+        if($accepted){
+            if((time() % 2) == 0){
+                EconomyAPI::getInstance()->reduceMoney($sender->getName(), $betAmount);
+                EconomyAPI::getInstance()->addMoney($opponentName, $betAmount);
+                $sender->getServer()->getPlayer($opponentName)->sendMessage(TextFormat::GREEN . "You WON the CoinFlip and got $" . $betAmount);
+                $sender->sendMessage(TextFormat::RED . "You LOST the CoinFlip and lost $" . $betAmount);
+            } else {
+                EconomyAPI::getInstance()->addMoney($sender->getName(), $betAmount);
+                EconomyAPI::getInstance()->reduceMoney($opponentName, $betAmount);
+                $sender->sendMessage(TextFormat::GREEN . "You WON the CoinFlip and got $" . $betAmount);
+                $sender->getServer()->getPlayer($opponentName)->sendMessage(TextFormat::RED . "You LOST the CoinFlip and lost $" . $betAmount);
+            }
+        } else {
+            $sender->sendMessage(TextFormat::RED . "You denied the CoinFlip request.");
+            $sender->getServer()->getPlayer($opponentName)->sendMessage(TextFormat::RED . $sender->getName() . " denied the CoinFlip request.");
+        }
+
+        unset(CoinFlip::$inp[$opponentName]);
+        unset(CoinFlip::$queue[strtolower($sender->getName())]);
     }
 }
