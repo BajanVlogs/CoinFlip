289fcab1a35cdd9665d0c130565bcadc6fbd0f0d
Lil Kev <kjolly544@gmail.com>
2024-04-11 01:03:23 -0400

    Update CoinFlip.php

--- src/BajanVlogs/CoinFlip.php
+++ src/BajanVlogs/CoinFlip.php
@@ -1,74 +1,46 @@
 <?php
 
-namespace BajanVlogs;
+declare(strict_types = 1);
 
-use pocketmine\event\player\PlayerQuitEvent;
+namespace BajanVlogs\CoinFlip;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\command\CommandSender;
-use pocketmine\command\Command;
-use pocketmine\Player;
-use pocketmine\utils\TextFormat;
-use pocketmine\utils\TextFormat as TF;
-use pocketmine\event\player\PlayerItemConsumeEvent;
-use pocketmine\entity\Effect;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\entity\Zombie;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\entity\EntityDeathEvent;
-use pocketmine\event\block\BlockBreakEvent;
-use pocketmine\math\Vector3;
-use pocketmine\entity\Entity;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\scheduler\CallbackTask;
-use pocketmine\level\Level;
-use pocketmine\level\Position;
-use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\event\player\PlayerLoginEvent;
-use pocketmine\event\player\PlayerPreLoginEvent;
-use pocketmine\math\Vector2;
-use pocketmine\utils\Config;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\item\Item;
-use pocketmine\event\player\PlayerItemHeldEvent;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\nbt\tag\Compound;
-use pocketmine\nbt\tag\Enum;
-use pocketmine\nbt\tag\Double;
-use pocketmine\nbt\tag\Short;
-use pocketmine\item\enchantment\Enchantment;
-use pocketmine\event\block\BlockPlaceEvent;
-use pocketmine\tile\Chest;
-use pocketmine\block\Block;
-use pocketmine\event\player\PlayerKickEvent;
-use pocketmine\event\player\PlayerCommandPreprocessEvent;
 
-// Use fully qualified namespace for Float class
-use pocketmine\nbt\tag\Float as PMFloat;
 
-class CoinFlip extends PluginBase implements Listener
-{
-    /** @var string[] */
-    public static $queue = [];
-    public static $inp = [];
-
-    public function onEnable(){
-        $this->getLogger()->info('CoinFlip Loaded');
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getServer()->getCommandMap()->register("cf", new CoinFlipCommand("cf"));
+class CoinFlip extends PluginBase implements Listener {
+    
+    public function onEnable() : void{
     }
 
-    public function onDisable(){
-        $this->getLogger()->info('CoinFlip Disabled');
+    public function onDisable() : void{
     }
 
-    public function onLeave(PlayerQuitEvent $ev){
-        if(isset(CoinFlip::$queue[strtolower($ev->getPlayer()->getName())])){
-            $ev->getPlayer()->getServer()->getPlayer(CoinFlip::$queue[strtolower($ev->getPlayer()->getName())][0])->sendMessage(TextFormat::RED . $ev->getPlayer()->getName() . " Left the game.");
-            unset(CoinFlip::$inp[CoinFlip::$queue[strtolower($ev->getPlayer()->getName())][0]]);
-            unset(CoinFlip::$queue[strtolower($ev->getPlayer()->getName())]);
+    public function Flip(){
+
+        $result = mt_rand(0,1);//1 tails : 0 heads
+
+        return $result;
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+        switch ($command->getName()) {
+          case "coinflip":
+            
+            if( $this->flip() == 1){
+                
+                $this->getServer()->broadcastMessage("§6>> §eA coin flip resulted in: §l§eTails");
+                
+            } else {
+                
+                $this->getServer()->broadcastMessage("§6>> §eA coin flip resulted in: §l§eHeads");
+
+            }
+
+            break;
         }
+        return true;
     }
 }
-
